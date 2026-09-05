b6d80f7c6f67e882822fb193974e6e5683d969d2
yenary <almeidayenary@gmail.com>
2026-09-05 11:04:09 -0500

    subiendo actividad 2

--- coneccion_bd.py
+++ coneccion_bd.py
@@ -1,13 +1,17 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def obtener_coneccion():
     try:
         conexion= mysql.connector.connect(
-            host="localhost",
-            database="contador",
-            user="root",
-            password="root"
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD')
         )
         if conexion.is_connected():
             print("conexion exitosa a la base de datos")
